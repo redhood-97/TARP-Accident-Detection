7a2e08978f9d75891aad66fba9783fec93423a3b
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-13 04:34:34 +0530

    Changed mapping to Plotly

--- folium_mapping.py
+++ folium_mapping.py
@@ -1,18 +1,20 @@
-import folium
-import pandas as pd
- 
-data = pd.DataFrame({
-'lat':[-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
-'lon':[-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
-'name':['Buenos Aires', 'Paris', 'melbourne', 'St Petersbourg', 'Abidjan', 'Montreal', 'Nairobi', 'Salvador']
-})
-data
- 
+import googlemaps
+from datetime import datetime
+import json
 
-m = folium.Map(location=[20, 0], tiles="Mapbox Bright", zoom_start=2)
- 
+plotlykey="dLABb2R1baaRKtKYS5oG"
+gmaps = googlemaps.Client(key='AIzaSyB84VySbMwOjfLY5SYDdi07vmNeMuq7Zug')
 
-for i in range(0,len(data)):
-    folium.Marker([data.iloc[i]['lon'], data.iloc[i]['lat']], popup=data.iloc[i]['name']).add_to(m)
- 
-m.save('312_markers_on_folium_map1.html')
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+
+print (geocode_result[0].keys())
+
+
+# print (type(directions_result))
