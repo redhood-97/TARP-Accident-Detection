06a1c6f0a6fe5937504c25f6fcf23b63ed003993
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-15 00:00:06 +0530

    SMS sample of the code is added

--- sms.py
+++ sms.py
@@ -1,8 +1,6 @@
-# /usr/bin/env python
-# Download the twilio-python library from twilio.com/docs/libraries/python
+
 from twilio.rest import Client
 
-# Find these values at https://twilio.com/user/account
 account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 auth_token = "your_auth_token"
 
