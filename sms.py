7edc542af47c402883888ff691aee3a08fe35872
aakashvarma18 <aakashvarma18@gmail.com>
2018-02-21 05:25:37 +0530

    SMS system using twillio shows ERROR

--- sms.py
+++ sms.py
@@ -1,12 +1,16 @@
+#Author - Aakash Varma
+#Code_name - Glitch
+
+#This is sending SMS using twillio 
 
 from twilio.rest import Client
 
-account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-auth_token = "your_auth_token"
+account_sid = "ACfb32763b6b8d047b86df7534a70245e0"
+auth_token = "f4e3fd3210a3e056d355d17eec5dad82"
 
 client = Client(account_sid, auth_token)
 
 client.api.account.messages.create(
-    to="+12316851234",
-    from_="+15555555555",
-    body="Hello there!")
+    to="+91 76600 66152",
+    from_="+1 502-443-7038 ",
+    body="An Accident has occures at the location (")
