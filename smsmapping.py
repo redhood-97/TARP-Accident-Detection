d3a69526444a39321516e94e960ee5cd9146daac
aakashvarma18 <aakashvarma18@gmail.com>
2018-03-10 00:06:04 +0530

    Minor change

--- smsmapping.py
+++ smsmapping.py
@@ -5,7 +5,7 @@ from plotly.offline import plot
 from twilio.rest import Client
 
 account_sid = "ACfb32763b6b8d047b86df7534a70245e0"
-auth_token = "f4e3fd3210a3e056d355d17eec5dad82"
+auth_token = "###"
 
 client = Client(account_sid, auth_token)
 
